45d3abfc351349936855abd466fd8abe5dd17915
Ruben Boone <ruben.boone01@gmail.com>
2024-03-10 15:39:20 +0100

    better button

--- content.js
+++ content.js
@@ -1,13 +1,23 @@
-var text = document.body.getElementsByTagName("textarea")[0]
+if (window.location.href.includes("google"))
+{
+    var text = document.body.getElementsByTagName("textarea")[0]
 
-var buttondiv = document.createElement("div");
-var button = document.createElement("button");
-button.textContent = "AS";
-button.addEventListener('click', function() {
-    chrome.runtime.sendMessage({action: 'open_popup'});
-    event.preventDefault();
-});
-
-buttondiv.appendChild(button);
-
-text.parentNode.parentNode.insertBefore(buttondiv, text.parentNode)
+    var buttondiv = document.createElement("div");
+    buttondiv.style.paddingRight = "13px"
+    buttondiv.style.paddingLeft = "8px"
+    var button = document.createElement("img");
+    button.src = chrome.runtime.getURL("images/as_icon.png");
+    // button.textContent = "AS";
+    button.style.marginTop = "10px";
+    button.style.marginBottom = "10px";
+    button.width = 17
+    button.height = 17
+    button.addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: 'open_popup'});
+        event.preventDefault();
+    });
+    
+    buttondiv.appendChild(button);
+    
+    text.parentNode.parentNode.append(buttondiv)        
+}
